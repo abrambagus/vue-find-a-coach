bdf29f91ef932b519473700bbcb9e1cca9421ee5
abram <abrambagus@gmail.com>
2025-03-20 15:16:43 +0700

    storing request with vuex

--- src/pages/request/ContactCoach.vue
+++ src/pages/request/ContactCoach.vue
@@ -29,14 +29,21 @@ export default {
   methods: {
     submitForm() {
       this.formIsValid = true;
+      console.log('abram', this.email, this.message);
       if (
         this.email === '' ||
-        !this.email.includes('0') ||
+        !this.email.includes('@') ||
         this.message === ''
       ) {
         this.formIsValid = false;
         return;
       }
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id,
+      });
+      this.$router.replace('/coaches');
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
 
 const store = createStore({
-  modules: { coaches: coachesModule },
+  modules: { coaches: coachesModule, reqest: requestsModule },
   state() {
     return {
       userId: 'c3 ',
