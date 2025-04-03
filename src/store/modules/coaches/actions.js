5c4cb3356790f36aedce16602fc462a7ac6f35be
abram <abrambagus@gmail.com>
2025-04-03 12:00:09 +0700

    caching http request

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -31,7 +31,11 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       'https://vue-http-demo-b73b6-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json'
     );
@@ -57,5 +61,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimeStamp');
   },
 };
