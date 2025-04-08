91beb7e919fa9d6d579e6448972e848f625eb1ca
abram <abrambagus@gmail.com>
2025-04-08 15:55:57 +0700

    add loading spinner & error handling for signup

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -16,9 +16,8 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
-        responseData.message || 'Failed to authenticate.'
+        responseData.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
