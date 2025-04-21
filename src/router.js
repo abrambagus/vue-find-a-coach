6dea47ef43e1ba5f196897383dfabc0e25b55330
abram <abrambagus@gmail.com>
2025-04-21 15:23:29 +0700

    add lazy import

--- src/router.js
+++ src/router.js
@@ -1,13 +1,30 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/request/ContactCoach.vue';
-import RequestReceived from './pages/request/RequestReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/request/ContactCoach.vue';
+// import RequestReceived from './pages/request/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachDetail.vue')
+);
+const CoachRegistration = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachRegistration.vue')
+);
+const ContactCoach = defineAsyncComponent(() =>
+  import('./pages/request/ContactCoach.vue')
+);
+const RequestReceived = defineAsyncComponent(() =>
+  import('./pages/request/RequestReceived.vue')
+);
+const UserAuth = defineAsyncComponent(() =>
+  import('./pages/auth/UserAuth.vue')
+);
 
 const router = createRouter({
   history: createWebHistory(),
